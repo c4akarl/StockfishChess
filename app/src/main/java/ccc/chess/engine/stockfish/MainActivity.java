0a0ea331f819e93ad7ec43302cfe2c64a49a3f9f
Karl Schreiner <c4akarl@gmail.com>
2017-01-18 22:24:52 +0100

    version 2.0

---
+++ app/src/main/java/ccc/chess/engine/stockfish/MainActivity.java
@@ -0,0 +1,13 @@
+package ccc.chess.engine.stockfish;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		finish();
+	}
+}
---
+++ app/src/main/java/ccc/chess/engine/stockfish/StockfishEngineProvider.java
@@ -0,0 +1,6 @@
+package ccc.chess.engine.stockfish;
+
+import com.kalab.chess.enginesupport.ChessEngineProvider;
+
+public class StockfishEngineProvider extends ChessEngineProvider {
+}
