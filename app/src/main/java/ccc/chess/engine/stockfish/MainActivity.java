a8e5fe9cb522515ca2c0d80c022dd6897ec2a0dc
Karl Schreiner <c4akarl@gmail.com>
2020-04-29 19:28:10 +0200

    version 2.0 t1

--- app/src/main/java/ccc/chess/engine/stockfish/MainActivity.java
+++ app/src/main/java/ccc/chess/engine/stockfish/MainActivity.java
@@ -2,12 +2,19 @@ package ccc.chess.engine.stockfish;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		finish();
+		setContentView(R.layout.main_layout);
+
+		Log.i(TAG, "MainActivity, onCreate()");
+
+//		finish();
 	}
+
+	final String TAG = "MainActivity";
 }
