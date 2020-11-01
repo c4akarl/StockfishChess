f5e5564f38abe2df0bfd2bc285bb7c2f971382a6
Karl Schreiner <c4akarl@gmail.com>
2020-11-01 10:28:03 +0100

    version 2.3.t1

--- app/src/main/java/ccc/chess/engine/stockfish/MainActivity.java
+++ app/src/main/java/ccc/chess/engine/stockfish/MainActivity.java
@@ -3,6 +3,7 @@ package ccc.chess.engine.stockfish;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +13,8 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.main_layout);
 		webView = findViewById(R.id.info);
 		webView.loadUrl(engineInfo);
+		engineTitle = findViewById(R.id.engineTitle);
+		engineTitle.setText("Stockfish Engines  " + BuildConfig.VERSION_NAME);
 	}
 
 	@Override
@@ -23,5 +26,6 @@ public class MainActivity extends Activity {
 
 	WebView webView;
 	final String engineInfo = "file:///android_res/raw/engine_info";
+	TextView engineTitle = null;
 
 }
